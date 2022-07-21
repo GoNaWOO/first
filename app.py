4752289cbed6d55370f0c4732e16aab881b04ec5
GoNaWOO <109637776+GoNaWOO@users.noreply.github.com>
2022-07-21 12:04:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sqlite3
-
-
+import pandas as pd
+import os.path
 
 con = sqlite3.connect('db.db')
 cur = con.cursor()
@@ -18,19 +18,53 @@ if menu == '로그인':
     login_id = st.text_input('아이디',placeholder= '아이디를 입력하세요')
     login_pw = st.text_input('비밀번호', placeholder= '비밀번호를 입력하세요', type = 'password')
     login_btn = st.button('로그인')
+    st.sidebar.subheader('로그인')
+
     if login_btn:
         user_info = login_user(login_id, login_pw)
-        st.write(user_info[4], '님 환영합니다.')
-    st.sidebar.subheader('로그인')
+        file_name = './img/'+user_info[0]+'.jpg'
+
+        if os.path.exists(file_name):
+            st.sidebar.image(file_name)
+            st.write(user_info[4], '님 환영합니다.')
+        else:
+            st.write(user_info[4], '님 환영합니다.')
+            
+
+
 
 
 if menu == '회원가입':
-    st.subheader('회원가입')
-    st.sidebar.write('회원가입')
+    with st.form('my_form', clear_on_submit=True):
+        st.info('다음 양식을 모두 입력 후 제출합니다.')
+        uid = st.text_input('아이디', max_chars=12)
+        uname = st.text_input('성명', max_chars=10)
+        upwd = st.text_input('비밀번호', type='password')
+        upwd_chk = st.text_input('비밀번호 확인', type='password')
+        uage = st.text_input('나이')
+        ugender = st.radio('성별', options=['남', '여'], horizontal=True)
+
+        ubtn = st.form_submit_button('회원가입')
+        if ubtn:
+            if upwd != upwd_chk:
+                st.error('비밀번호가 일치하지 않습니다.')
+                st.stop()
+
+            cur.execute(f"INSERT INTO users(id, pwd, name, age, gender) VALUES ("
+                        f"'{uid}', '{upwd}', '{uname}','{uage}', '{ugender}')")
+            con.commit()
+            st.success('회원가입에 성공했습니다.')
+
+
+
+
 if menu == '회원목록':
     st.subheader('회원목록')
+    df = pd.read_sql('SELECT id, name,gender FROM users ', con)
+    st.dataframe(df)
     st.sidebar.write('회원목록')
 
+    st.video('https://www.youtube.com/watch?v=VcsDtJ-GhfE')
     
 
 
