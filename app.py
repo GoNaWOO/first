ad3b02fb7e1fa773520c0e108eedb91af8904ea1
GoNaWOO <109637776+GoNaWOO@users.noreply.github.com>
2022-07-22 12:03:24 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import sqlite3
 import pandas as pd
 import os.path
+import numpy as np
+import cv2
+from PIL import Image, ImageEnhance
+
 
 con = sqlite3.connect('db.db')
 cur = con.cursor()
@@ -10,7 +14,7 @@ def login_user(id, pw):
     cur.execute(f"SELECT * FROM users WHERE id='{id}' and pwd = '{pw}'")
     return cur.fetchone()
 
-menu = st.sidebar.selectbox('MENU', options=['로그인', '회원가입', '회원목록'])
+menu = st.sidebar.selectbox('MENU', options=['로그인', '회원가입', '회원목록','이미지 크롭',])
 
 if menu == '로그인':
     st.subheader('로그인')
@@ -29,7 +33,7 @@ if menu == '로그인':
             st.write(user_info[4], '님 환영합니다.')
         else:
             st.write(user_info[4], '님 환영합니다.')
-            
+
 
 
 
@@ -64,7 +68,57 @@ if menu == '회원목록':
     st.dataframe(df)
     st.sidebar.write('회원목록')
 
-    st.video('https://www.youtube.com/watch?v=VcsDtJ-GhfE')
+if menu == '이미지 크롭':
+    import streamlit as st
+    from streamlit_cropper import st_cropper
+    from PIL import Image
+
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+
+    # Upload an image and set some options for demo purposes
+    st.header("Image Cropper")
+    img_file = st.sidebar.file_uploader(label='Upload a file', type=['png', 'jpg'])
+    realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
+    box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
+    aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
+    aspect_dict = {
+        "1:1": (1, 1),
+        "16:9": (16, 9),
+        "4:3": (4, 3),
+        "2:3": (2, 3),
+        "Free": None
+    }
+    aspect_ratio = aspect_dict[aspect_choice]
+
+    if img_file:
+        img = Image.open(img_file)
+        if not realtime_update:
+            st.write("Double click to save crop")
+        # Get a cropped image from the frontend
+        cropped_img = st_cropper(img, realtime_update=realtime_update, box_color=box_color,
+                                 aspect_ratio=aspect_ratio)
+
+        # Manipulate cropped image at will
+        st.write("Preview")
+        _ = cropped_img.thumbnail((150, 150))
+        st.image(cropped_img)
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
     
 
 
